6a680a1df83213635f50c8c259948d0a062539ae
Iyad Saadeh <iyaadsaadeh@gmail.com>
2024-10-26 15:56:52 +0300

    models

---
+++ Teeth.Domain/Repository/IUserRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using Teeth.Domain.Models;
+
+namespace Teeth.Domain.Repository
+{
+    public interface IUserRepository
+    {
+        Task<List<User>> GetAlUsersAsync();
+
+        Task<User> GetByIdAsync(int id);
+
+        Task<User> CreateAsync(User user);
+
+        Task<int> UpdateAsync(int id, User user);
+
+        Task<int> DeteteAsync(int id);
+    }
+}
